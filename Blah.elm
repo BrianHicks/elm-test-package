6a8ab52ddddfb55e180535f0568eee249e07f7e2
Brian Hicks <brian@brianthicks.com>
2017-08-29 13:52:12 -0500

    initial commit

---
+++ Blah.elm
@@ -0,0 +1,14 @@
+module Blah exposing (blah)
+
+{-| Very Blah
+
+@docs blah
+
+-}
+
+
+{-| blah does nothing. To be honest, it's more of a "meh".
+-}
+blah : String
+blah =
+    "blah"
---
+++ README.md
@@ -0,0 +1,1 @@
+Just an awful test package
